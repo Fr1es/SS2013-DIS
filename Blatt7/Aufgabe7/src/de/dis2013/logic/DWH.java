4137daeebf6adfddaa744d026315717ec323d4fb
Robert Heinecke <robert.heinecke@informatik.uni-hamburg.de>
2013-06-23 23:11:54 +0200

    wtf.

--- Blatt7/Aufgabe7/src/de/dis2013/logic/DWH.java
+++ Blatt7/Aufgabe7/src/de/dis2013/logic/DWH.java
@@ -15,6 +15,9 @@ public class DWH {
 	 */
 	public DWH() {
 		attributes = new ArrayList<String>();
+		attributes.add("region");
+		attributes.add("time");
+		attributes.add("product");
 	}
 	
 	public String toString() {
@@ -23,24 +26,32 @@ public class DWH {
 	}
 	
 	public void drillDown(String attributeName) {
-		
-		// TODO: check whether drill down is possible for this attribute
-		if (true) {
-			// TODO: enable drill down for an attribute
-		} else {
-			// no drill down possible for this attribute
-			return;
+		for (int i=0; i<=attributes.size(); i++) {
+			if (attributes.get(i) == attributeName) {
+				// TODO: check whether drill down is possible for this attribute
+				
+				// TODO: do drill down for an attribute
+				attributes.set(i, ""); // replace empty string with upper level
+				return;
+			}
 		}
+		
+		// no drill down possible for this attribute
+		return;
 	}
 	
 	public void rollUp(String attributeName) {
-		
-		// TODO: check whether roll up is possible for this attribute
-		if (true) {
-			// TODO: enable roll up for an attribute
-		} else {
-			// no roll up possible for this attribute
-			return;
+		for (int i=0; i<=attributes.size(); i++) {
+			if (attributes.get(i) == attributeName) {
+				// TODO: check whether roll up is possible for this attribute
+				
+				// TODO: do roll up for an attribute (i.e. modify current attribute state)
+				attributes.set(i, ""); // replace empty string with upper level
+				return;
+			}
 		}
+		
+		// no roll up possible for this attribute
+		return;
 	}
 }
