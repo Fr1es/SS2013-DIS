b9061c09fca21d0bb74131858a2bc3639cc855ed
Robert Heinecke <robert.heinecke@informatik.uni-hamburg.de>
2013-06-23 23:35:59 +0200

    This is BS.

--- Blatt7/Aufgabe7/src/de/dis2013/logic/DWH.java
+++ Blatt7/Aufgabe7/src/de/dis2013/logic/DWH.java
@@ -10,48 +10,67 @@ public class DWH {
 	// list of current attribute states
 	private ArrayList<String> attributes;
 	
+	private ArrayList<String> place;
+	private ArrayList<String> time;
+	private ArrayList<String> product;
+	
 	/**
 	 * Constructor
 	 */
 	public DWH() {
+		// list of possible place hierarchies
+		place = new ArrayList<String>();
+		place.add("name");
+		place.add("town");
+		place.add("region");
+		place.add("country");
+		
+		// list of possible time hierarchies
+		time = new ArrayList<String>();
+		time.add("dayID");
+		time.add("month");
+		time.add("quarter");
+		time.add("year");
+		
+		// list of possible product hierarchies
+		product = new ArrayList<String>();
+		product.add("name");
+		product.add("productGroup");
+		product.add("productFamily");
+		product.add("productCategory");
+		
+		// list of current hierarchies
 		attributes = new ArrayList<String>();
-		attributes.add("region");
-		attributes.add("time");
-		attributes.add("product");
+		attributes.add("town"); // place - i.e. name, town, region, country
+		attributes.add("quarter"); // time - i.e. dayID, month, quarter, year
+		attributes.add("name"); // product - i.e. name, productGroup, productFamily, productCategory
 	}
 	
 	public String toString() {
-		// TODO: make this return the current state (dependent on current attribute states)
+		// TODO: make this return the current state (dependent on current hierarchies)
 		return null;
 	}
 	
+	/**
+	 * 
+	 * @param attributeName
+	 */
 	public void drillDown(String attributeName) {
-		for (int i=0; i<=attributes.size(); i++) {
-			if (attributes.get(i) == attributeName) {
-				// TODO: check whether drill down is possible for this attribute
-				
-				// TODO: do drill down for an attribute
-				attributes.set(i, ""); // replace empty string with upper level
-				return;
-			}
-		}
+	
+	
+		// TODO: check whether drill down is possible for this attribute
 		
-		// no drill down possible for this attribute
+		// TODO: do drill down for an attribute
+		
 		return;
 	}
 	
 	public void rollUp(String attributeName) {
-		for (int i=0; i<=attributes.size(); i++) {
-			if (attributes.get(i) == attributeName) {
-				// TODO: check whether roll up is possible for this attribute
-				
-				// TODO: do roll up for an attribute (i.e. modify current attribute state)
-				attributes.set(i, ""); // replace empty string with upper level
-				return;
-			}
-		}
+
+		// TODO: check whether roll up is possible for this attribute
 		
-		// no roll up possible for this attribute
+		// TODO: do roll up for an attribute (i.e. modify current attribute state)
+
 		return;
 	}
 }
